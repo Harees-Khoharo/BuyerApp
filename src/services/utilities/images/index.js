515ff8399813f8d62a6d6a7f7b696d673ae3d1c4
Mustafa ilahi <mustafailahi586@gmail.com>
2023-12-15 12:52:31 -0800

    help center images issue fixed

--- src/services/utilities/images/index.js
+++ src/services/utilities/images/index.js
@@ -1,59 +1,67 @@
-import dhl from '../../../assets/dhl.png';
-import usps from '../../../assets/usps.png';
-import fedex from '../../../assets/fedex.png';
-import mastercard from '../../../assets/mastercard.png';
-import backArrow from '../../../assets/backArrow.png';
-import checkboxon from '../../../assets/checkboxon.png';
-import checkboxoff from '../../../assets/checkboxoff.png';
-import card from '../../../assets/card.png';
-import cardTwo from '../../../assets/cardTwo.png';
-import cartTwo from '../../../assets/cartTwo.png';
-import plus from '../../../assets/plus.png';
-import mastercardTwo from '../../../assets/mastercardTwo.png';
-import rightArrow from '../../../assets/rightArrow.png';
-import bags from '../../../assets/bags.png';
-import camera from '../../../assets/camera.png';
-import cart from '../../../assets/cart.png';
-import drink from '../../../assets/drink.png';
-import dropdown from '../../../assets/dropdown.png';
-import filter from '../../../assets/filter.png';
-import grocery from '../../../assets/grocery.png';
-import heart from '../../../assets/heart.png';
-import image from '../../../assets/image.png';
-import imageTwo from '../../../assets/imageTwo.png';
-import search from '../../../assets/search.png';
-import location from '../../../assets/location.png';
-import rating from '../../../assets/rating.png';
-import electronics from '../../../assets/electronics.png';
-import appliances from '../../../assets/appliances.png';
-import toiletries from '../../../assets/toiletries.png';
-import wear from '../../../assets/wear.png';
-import furniture from '../../../assets/furniture.png';
-import bellicon from '../../../assets/bellicon.png';
-import payment from '../../../assets/payment.png';
-import order from '../../../assets/order.png';
-import message from '../../../assets/message.png';
-import logout from '../../../assets/logout.png';
-import invite from '../../../assets/invite.png';
-import setting from '../../../assets/setting.png';
-import help from '../../../assets/help.png';
-import privacy from '../../../assets/privacy.png';
-import profileEdit from '../../../assets/profileEdit.png';
-import balance from '../../../assets/balance.png';
-import address from '../../../assets/address.png';
-import profile from '../../../assets/profile.png';
-import threeDot from '../../../assets/threeDot.png';
-import book from '../../../assets/book.png';
-import visaCard from '../../../assets/visaCard.png';
-import calender from '../../../assets/calender.png';
-import downArrow from '../../../assets/downArrow.png';
-import flag from '../../../assets/flag.png';
-import email from '../../../assets/email.png';
-import notiCross from '../../../assets/notiCross.png';
-import notiHeader from '../../../assets/notiHeader.png';
-import notiTick from '../../../assets/notiTick.png';
-import notiTicket from '../../../assets/notiTicket.png';
-import notiWallet from '../../../assets/notiWallet.png';
+import dhl from "../../../assets/dhl.png";
+import usps from "../../../assets/usps.png";
+import fedex from "../../../assets/fedex.png";
+import mastercard from "../../../assets/mastercard.png";
+import backArrow from "../../../assets/backArrow.png";
+import checkboxon from "../../../assets/checkboxon.png";
+import checkboxoff from "../../../assets/checkboxoff.png";
+import card from "../../../assets/card.png";
+import cardTwo from "../../../assets/cardTwo.png";
+import cartTwo from "../../../assets/cartTwo.png";
+import plus from "../../../assets/plus.png";
+import mastercardTwo from "../../../assets/mastercardTwo.png";
+import rightArrow from "../../../assets/rightArrow.png";
+import bags from "../../../assets/bags.png";
+import camera from "../../../assets/camera.png";
+import cart from "../../../assets/cart.png";
+import drink from "../../../assets/drink.png";
+import dropdown from "../../../assets/dropdown.png";
+import filter from "../../../assets/filter.png";
+import grocery from "../../../assets/grocery.png";
+import heart from "../../../assets/heart.png";
+import image from "../../../assets/image.png";
+import imageTwo from "../../../assets/imageTwo.png";
+import search from "../../../assets/search.png";
+import location from "../../../assets/location.png";
+import rating from "../../../assets/rating.png";
+import electronics from "../../../assets/electronics.png";
+import appliances from "../../../assets/appliances.png";
+import toiletries from "../../../assets/toiletries.png";
+import wear from "../../../assets/wear.png";
+import furniture from "../../../assets/furniture.png";
+import bellicon from "../../../assets/bellicon.png";
+import payment from "../../../assets/payment.png";
+import order from "../../../assets/order.png";
+import message from "../../../assets/message.png";
+import logout from "../../../assets/logout.png";
+import invite from "../../../assets/invite.png";
+import setting from "../../../assets/setting.png";
+import help from "../../../assets/help.png";
+import privacy from "../../../assets/privacy.png";
+import profileEdit from "../../../assets/profileEdit.png";
+import balance from "../../../assets/balance.png";
+import address from "../../../assets/address.png";
+import profile from "../../../assets/profile.png";
+import threeDot from "../../../assets/threeDot.png";
+import book from "../../../assets/book.png";
+import visaCard from "../../../assets/visaCard.png";
+import calender from "../../../assets/calender.png";
+import downArrow from "../../../assets/downArrow.png";
+import flag from "../../../assets/flag.png";
+import email from "../../../assets/email.png";
+import notiCross from "../../../assets/notiCross.png";
+import notiHeader from "../../../assets/notiHeader.png";
+import notiTick from "../../../assets/notiTick.png";
+import notiTicket from "../../../assets/notiTicket.png";
+import notiWallet from "../../../assets/notiWallet.png";
+import support from "../../../assets/support.png";
+import twitter from "../../../assets/twitter.png";
+import facebook from "../../../assets/facebook.png";
+import insta from "../../../assets/insta.png";
+import website from "../../../assets/website.png";
+import whatsApp from "../../../assets/whatsApp.png";
+import upArrow from "../../../assets/upArrow.png";
+import leftArrowIcon from "../../../assets/leftArrowIcon.png";
 
 const images = {
   dhl,
@@ -112,6 +120,14 @@ const images = {
   notiTicket,
   notiWallet,
   cartTwo,
+  support,
+  twitter,
+  facebook,
+  insta,
+  website,
+  whatsApp,
+  upArrow,
+  leftArrowIcon,
 };
 
 export default images;
